926927da88063f5b79b2c084d8ba59fcd4f8ee3e
brunsson <brunsson@hotmail.fr>
2012-03-24 23:46:57 +0100

    Add french version

--- plugins/displaypicture.py
+++ plugins/displaypicture.py
@@ -23,6 +23,7 @@ class define(Plugin):
     
     @register("de-DE", "(zeig mir|zeige|zeig).*(bild|zeichnung) (vo. ein..|vo.|aus)* ([\w ]+)")
     @register("en-US", "(display|show me|show).*(picture|image|drawing|illustration) (of|an|a)* ([\w ]+)")
+    @register("fr-FR", "(Montre|Afficher|Affiche).*?(photos|photo|image|dessin|illustration)?.*(d'un|de|un|a)* ([\w ]+)")
     def defineword(self, speech, language, regex):
         Title = regex.group(regex.lastindex)
         Query = urllib.quote_plus(Title.encode("utf-8"))
